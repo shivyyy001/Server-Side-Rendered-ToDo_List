a4c2b97c3702e2f1c2035cdc1f5a764ddd2b3d80
shivyyy001 <kaushalshivansh6@gmail.com>
2021-12-11 01:23:04 +0530

    first commit

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const app = express();
+var path = require("path");
 
-let tasks=[];
-let publicPath=__dirname+'/public';
+let tasks = [];
+let publicPath = path.join(__dirname, "/public");
 
 app.use(express.static(publicPath));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-  res.render("index",{
-      tasks
+  res.render("index", {
+    tasks,
   });
 });
 
 app.post("/", (req, res) => {
-    tasks.push(req.body.task);
-    res.redirect('/');
-  });
-
-app.listen(3000, () => { 
-  console.log("listening on port 3000");
+  tasks.push(req.body.task);
+  res.redirect("/");
 });
 
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
